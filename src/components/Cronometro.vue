18a8d5346ec05370b0ceb914ca17491083b857a7
Felipe <fsoares@autodoc.com.br>
2023-10-27 12:01:11 -0300

    refactor: extraindo comportamento do formulário

---
+++ src/components/Cronometro.vue
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import { defineComponent } from 'vue';
+
+  export default defineComponent({
+    name: 'Cronometro',
+    props: {
+      temporizador: {
+        type: Number,
+        default: 0
+      }
+    },
+    computed:{
+      tempoFormatado(): string {
+        return new Date(this.temporizador * 1000).toISOString().substr(11,8);
+      }
+    },
+  })
+</script>
+
+<template>
+  <strong>{{ tempoFormatado }}</strong>
+</template>
+
+<style scoped>
+
+</style>
