faef2babcdcf58d3ff7cc2b82b116bb882e1a113
Felipe <fsoares@autodoc.com.br>
2023-10-27 16:49:07 -0300

    refactor: extraindo div box para um componente

---
+++ src/components/Box.vue
@@ -0,0 +1,16 @@
+<script  lang="ts">
+  import { defineComponent } from 'vue';
+  export default defineComponent({
+    name: 'Box',
+  })
+</script>
+
+<template>
+  <div class="box has-text-weight-bold">
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped>
+  .box {  background: #FAF0CA;}
+</style>
