396e75d4347f6410b961be37a0009a96b3f1d9fc
Felipe <fsoares@autodoc.com.br>
2023-10-30 14:08:38 -0300

    feat: dicionando modo escuro

--- src/components/BarraLateral.vue
+++ src/components/BarraLateral.vue
@@ -4,14 +4,36 @@
     <h1>
       <img src="../assets/logo.png" alt="logo">
     </h1>
+    <Button :evento-click="alterarTema" :class="button">
+      {{ labelButton }}
+    </Button>
   </header>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import Button from "@/components/Button.vue";
 
 export default defineComponent({
   name: 'BarraLateral',
+  components: {Button},
+  emits: ['aoTemaAlterado'],
+  data() {
+    return {
+      temaEscuro: false,
+    }
+  },
+  computed: {
+    labelButton () {
+      return this.temaEscuro ? 'Desativar modo escuro' : 'Ativar modo escuro'
+    },
+  },
+  methods: {
+    alterarTema() {
+      this.temaEscuro = !this.temaEscuro
+      this.$emit('aoTemaAlterado', this.temaEscuro)
+    }
+  }
 })
 </script>
 
@@ -21,6 +43,7 @@ export default defineComponent({
     background: #0d3b66;
     width: 100%;
     height: 100vh;
+    text-align: center;
   }
   @media only screen and (max-width: 768px) {
     header {
@@ -28,4 +51,5 @@ export default defineComponent({
       height: auto;
     }
   }
+
 </style>
